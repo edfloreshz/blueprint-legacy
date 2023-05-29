f3d347fbe679db6fb631cb25db96b890f883fea0
Eduardo Flores <edfloreshz@gmail.com>
2023-05-28 21:19:52 -0700

    Updated metainfo

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,9 @@
-pub const APP_ID: &str = "dev.edfloreshz.Blueprint";
+pub const APP_ID: &str = "dev.edfloreshz.Blueprint.Devel";
 pub const GETTEXT_PACKAGE: &str = "blueprint";
-pub const LOCALEDIR: &str = "/usr/local/share/locale";
+pub const LOCALEDIR: &str = "/app/share/locale";
 #[allow(unused)]
-pub const PKGDATADIR: &str = "/usr/local/share/blueprint";
-pub const PROFILE: &str = "";
+pub const PKGDATADIR: &str = "/app/share/blueprint";
+pub const PROFILE: &str = "Devel";
 #[allow(unused)]
-pub const RESOURCES_FILE: &str = concat!("/usr/local/share/blueprint", "/resources.gresource");
-pub const VERSION: &str = "0.1.0";
+pub const RESOURCES_FILE: &str = concat!("/app/share/blueprint", "/resources.gresource");
+pub const VERSION: &str = "0.1.0-357bb2f";
